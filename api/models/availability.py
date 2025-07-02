1fa43296b65a0604a90ac7e853998a92597607e5
Wallace Freitas <103138773+Baku-Stark@users.noreply.github.com>
2025-07-02 19:33:16 -0300

    fix: Resolução do problema 'ColumnElement[bool]'

--- api/models/availability.py
+++ api/models/availability.py
@@ -1,13 +1,15 @@
-from sqlalchemy import Column, String, DateTime, Integer
-from sqlalchemy.dialects.postgresql import UUID
+from pydantic.v1 import UUID4
+from sqlalchemy import String, DateTime, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
 from api.db.database import Base
 
 class AvailabilitySlot(Base):
     __tablename__ = "availability_slots"
     __table_args__ = {"extend_existing": True}
 
-    id = Column(Integer, primary_key=True)
-    mentor_id = Column(UUID(as_uuid=True), nullable=False)
-    start_time = Column(DateTime)
-    end_time = Column(DateTime)
-    timezone = Column(String(50))
+    id : Mapped[String] = mapped_column(Integer, primary_key=True)
+    mentor_id : Mapped[UUID4] = mapped_column(ForeignKey('users.id'))
+    start_time : Mapped[DateTime] = mapped_column(DateTime)
+    end_time : Mapped[DateTime] = mapped_column(DateTime)
+    timezone : Mapped[String] = mapped_column(String(50))
