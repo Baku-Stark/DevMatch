00138e94690ada988c32f3b489bd2fb8f48abaa7
Wallace Freitas <103138773+Baku-Stark@users.noreply.github.com>
2025-08-29 06:17:51 -0300

    feat: SASS and Global Variables

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.tsx'
-import './assets/css/index.css'
+import './assets/css/output.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
