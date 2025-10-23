b5e6b520f10255cdb6035e676ac7cb4a8122cf17
Wallace Freitas <103138773+Baku-Stark@users.noreply.github.com>
2025-10-23 19:20:56 -0300

    fix: remove sensite info

--- frontend/src/services/firebase.ts
+++ frontend/src/services/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { ENV } from "../config/env";
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDZ3g-XMBKZoCQvdZ0UpO3NQt_wf5CQ_Js",
-  authDomain: "devmatch-auth-521a1.firebaseapp.com",
-  projectId: "devmatch-auth-521a1",
-  storageBucket: "devmatch-auth-521a1.firebasestorage.app",
-  messagingSenderId: "515640480811",
-  appId: "1:515640480811:web:024e8f9120751c1bd3466e",
-  measurementId: "G-V0XYMQN2TX"
+  apiKey: ENV.API_KEY,
+  authDomain: ENV.AUTH_DOMAIN,
+  projectId: ENV.PROJECT_ID,
+  storageBucket: ENV.STORAGE_BUCKET,
+  messagingSenderId: ENV.MESSAGING_SENDER_ID,
+  appId: ENV.APP_ID,
+  measurementId: ENV.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
